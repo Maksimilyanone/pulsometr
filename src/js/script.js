a4748337e464b77516b5e86405149b662adf1f85
Maksim <jambojack198@gmail.com>
2023-03-07 19:54:17 +0300

    add corusel ver_1

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.corusel__iner').slick({
+        speed: 1200,
+        prevArrow:'<button type="button" class="slick-prev"><img src="../img/picture/chevron-left.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../img/picture/chevron-right.png"></img></button>'
+    });
+  });
